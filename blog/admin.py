0f4db34877b2637b90c074e0e76b5338214b2000
Gustavo Ewald <ewaldgustavo@gmail.com>
2025-05-05 00:19:09 -0300

    Django-admin config

--- blog/admin.py
+++ blog/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content')
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
